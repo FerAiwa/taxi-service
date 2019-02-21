ca04aa8639f2c866bce5a6fe22018412ac0fab35
Fer Ayguavives <47696430+FerAiwa@users.noreply.github.com>
2019-02-21 15:43:38 +0100

    Add files via upload

---
+++ _models/ride.js
@@ -0,0 +1,11 @@
+export class Ride {
+
+  constructor({id, to, from, cost, scale}) {
+    this.id = id;
+    this.to = to;
+    this.from = from;
+    this.cost = cost;
+    this.scale = scale;
+  }
+
+}
---
+++ _models/user.js
@@ -0,0 +1,8 @@
+export class User {
+
+  constructor(name, permissions) {
+    this.name = name;
+    this.permissions = permissions;
+  }
+
+}
