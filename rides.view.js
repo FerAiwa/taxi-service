8171b8fc6505c572798d647e5b7c451f0a92f0d8
Fer Ayguavives <fer.aiwa@gmail.com>
2019-03-07 00:22:29 +0100

    Update rides.view.js

--- rides.view.js
+++ rides.view.js
@@ -1,4 +1,4 @@
-import { EventEmitter } from "event-emitter.js";
+import { EventEmitter } from "./event-emitter.js";
 export class RidesView extends EventEmitter {
   constructor(hostElement) {
     super();
